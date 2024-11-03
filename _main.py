03847dfa4581df8d048d15c4d0f31dc7c4556708
J <55432390+Milerej@users.noreply.github.com>
2024-11-03 18:26:56 +0800

    Update _main.py

--- _main.py
+++ _main.py
@@ -486,7 +486,7 @@ if selected_page == "Ask DGP":
 
 
 # Content for About Us
-elif page == "About Us":
+elif selected_page == "About Us":
     st.title("About Us")
     st.write("""
              
@@ -568,7 +568,7 @@ The chatbot will include the following features:
     """)
 
 # Content for Methodology
-elif page == "Methodology":
+elif selected_page == "Methodology":
     st.title("Methodology")
     st.image('Flow1.PNG')
 
